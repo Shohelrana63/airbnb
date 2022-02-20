f86fd4be3c59559ec88347532ee1a98c7cc80f66
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2022-02-21 00:37:04 +0600

    header done

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import {
+    SearchIcon,
+    GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    UsersIcon
+} from '@heroicons/react/solid';
 
 function Header() {
   return (
-    <div>
-      <h1>welcome to airbnb</h1>
-    </div>
+    <header className="sticky top-0 x-50 grid grid-cols-3
+    bg-white shadow-md p-5 md:px-10">
+      {/* Left */}
+      <div className="relative flex items-center h-10
+      cursor-pointer my-auto">
+          <Image src="https://links.papareact.com/qd3"
+           layout='fill'
+           objectFit='contain'
+           objectPosition="left"
+           alt="" 
+           />
+      </div>
+
+      {/* Middle */}
+      <div className="flex items-center md:border-2 rounded-full py-2">
+          <input 
+          className="flex-grow pl-5 bg-transparent
+           outline-none md:shadow-sm text-sm text-gray-600 
+           placeholder-gray-480"
+           type="text" placeholder="Start your search" />
+           <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 rounded-full text-white
+           p-2 cursor-pointer md:mx-2"/>
+      </div>
+
+      {/* Right */}
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+          <p className="hidden md:inline cursor-pointer">Become a host</p>
+          <GlobeAltIcon className="h-6 cursor-pointer"/>
+          <div className="flex items-center space-x-4 border-2 p-2 rounded-full">
+            <MenuIcon className="h-6"/>
+            <UserCircleIcon className="h-6"/>
+          </div>
+      </div>
+      
+    </header>
   )
 }
 
-export default Header
+export default Header;
+
